643364d48b2b3bafe63c481299700db2729ccea2
vasyldorosh <dorosh_2009@meta.ua>
2022-02-15 12:05:41 +0200

    Update index.php

--- resources/blog/index.php
+++ resources/blog/index.php
@@ -7,21 +7,15 @@ use Vasyldorosh\PbnSatellite\PostRender;
 
 $alias = isset($_GET['alias']) ? $_GET['alias'] : null;
 
-if (empty($alias)) {
-    $items = (new PostStorage)->getPosts();
-    echo (new PostRender)->renderList($items);
+$post = $alias ? ((new PostStorage)->getPost($_GET['alias'])) : null;
+if (empty($post)) {
+	header("HTTP/1.0 404 Not Found");
+	echo (new PostRender)->renderItem([
+		'title' => 404,
+		'seo_title' => 404,
+		'seo_description' => 404,
+		'content' => 'Page not found',
+	]);
 } else {
-
-    $post = $alias ? ((new PostStorage)->getPost($_GET['alias'])) : null;
-    if (empty($post)) {
-        header("HTTP/1.0 404 Not Found");
-        echo (new PostRender)->renderItem([
-            'title' => 404,
-            'seo_title' => 404,
-            'seo_description' => 404,
-            'content' => 'Page not found',
-        ]);
-    } else {
-        echo (new PostRender)->renderItem($post);
-    }
+	echo (new PostRender)->renderItem($post);
 }

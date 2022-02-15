6cb73ee3782d602423f974158f1d06d3f8ecf7f0
vasyldorosh <dorosh_2009@meta.ua>
2022-02-15 10:54:44 +0200

    Update Installer.php

--- src/Installer.php
+++ src/Installer.php
@@ -10,16 +10,23 @@ class Installer
         $source = __DIR__ . '/../resources/blog';
         $dest = __DIR__ . '/../../../../blog';
 
-        mkdir($dest, 0755);
+        if (!is_dir($dest)) {
+            mkdir($dest, 0755);
+        }
+        
         foreach (
             $iterator = new \RecursiveIteratorIterator(
                 new \RecursiveDirectoryIterator($source, \RecursiveDirectoryIterator::SKIP_DOTS),
                 \RecursiveIteratorIterator::SELF_FIRST) as $item
         ) {
             if ($item->isDir()) {
-                mkdir($dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName());
+                if (!is_dir($dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName())) {
+                 mkdir($dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName());
+                }    
             } else {
-                copy($item, $dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName());
+                if (!is_file($dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName())) {
+                 copy($item, $dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName());
+                }    
             }
         }
 
